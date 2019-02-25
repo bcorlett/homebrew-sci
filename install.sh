1e5e36c778deb666e152834b4f6c27b8fd8ed9ca
B Corlett <bcorlett@whoi.edu>
2019-02-25 16:22:31 -0500

    found issue for netcdf -- require rewriting nf-config file (edited here)

--- install.sh
+++ install.sh
@@ -22,12 +22,23 @@ brew install nco
 brew install --cc=clang openmpi
 
 # download coawst to folder (svn comes standard on mac)
-svn 
+mkdir /usr/local/Coawst
+cd /usr/local/Coawst/
+#svn checkout --username myusrname https://coawstmodel.sourcerepo.com/coawstmodel/COAWST .
 
 # within MCT folder, run the following:
 brew diy --version=2.6.0 --name=mct
 ./configure --prefix=/usr/local/Cellar/mct/2.6.0
 make && make install
 brew link mct
+
+# within SCRIP_COAWST folder, run the following:
+brew diy --name=scrip_coawst
+./configure --prefix=/usr/local/Cellar/scrip_coawst/
+make && make install
+brew link scrip_coawst
+
+# copy modified nf-config file
+cp nf-config /usr/local/Cellar/netcdf/4.6.2/bin/nf-config
 
 
